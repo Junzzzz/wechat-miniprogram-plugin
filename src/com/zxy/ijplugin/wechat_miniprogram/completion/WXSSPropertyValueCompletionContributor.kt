743f87ab6d1b5c50fc60caf0e8d645698e5425dd
2544682494@qq.com <2544682494@qq.com>
2019-11-03 16:44:58 +0800

    增加一些完成贡献者的代码，还没弄明白

--- src/com/zxy/ijplugin/wechat_miniprogram/completion/WXSSPropertyValueCompletionContributor.kt
+++ src/com/zxy/ijplugin/wechat_miniprogram/completion/WXSSPropertyValueCompletionContributor.kt
@@ -4,13 +4,27 @@ import com.intellij.codeInsight.AutoPopupController
 import com.intellij.codeInsight.completion.*
 import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.patterns.PlatformPatterns
+import com.intellij.psi.css.impl.util.table.CssDescriptorsUtil
 import com.intellij.psi.css.impl.util.table.CssElementDescriptorFactory
+import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.util.ProcessingContext
 import com.zxy.ijplugin.wechat_miniprogram.lang.wxss.WXSSLanguage
+import com.zxy.ijplugin.wechat_miniprogram.lang.wxss.psi.WXSSStyleStatement
 import com.zxy.ijplugin.wechat_miniprogram.lang.wxss.psi.WXSSTypes
 
 
 class WXSSPropertyValueCompletionContributor : CompletionContributor() {
+
+    companion object {
+        private val WXSS_PROPERTY_NAMES by lazy {
+            CssDescriptorsUtil.getAllPropertyDescriptors(null).asSequence().map { it.id }.distinct()
+                    .filter { !it.startsWith("-") }
+                    .filter { !it.startsWith("mso") }
+                    .filter { !it.contains("_") }
+                    .toList()
+        }
+    }
+
     init {
         extend(
                 CompletionType.BASIC,
@@ -22,14 +36,17 @@ class WXSSPropertyValueCompletionContributor : CompletionContributor() {
                             context: ProcessingContext,
                             resultSet: CompletionResultSet
                     ) {
-                        resultSet.addElement(
-                                LookupElementBuilder.create("Hello").withInsertHandler { insertionContext, lookupElement ->
-                                    val editor = insertionContext.editor
-                                    val offset = editor.caretModel.offset
-                                    insertionContext.document.insertString(offset, ": ;")
-                                    editor.caretModel.moveToOffset(offset + 2)
-                                    AutoPopupController.getInstance(insertionContext.project)
-                                            .autoPopupMemberLookup(insertionContext.editor, null)
+
+                        resultSet.addAllElements(
+                                WXSS_PROPERTY_NAMES.map {
+                                    LookupElementBuilder.create(it).withInsertHandler { insertionContext, _ ->
+                                        val editor = insertionContext.editor
+                                        val offset = editor.caretModel.offset
+                                        insertionContext.document.insertString(offset, ": ;")
+                                        editor.caretModel.moveToOffset(offset + 2)
+                                        AutoPopupController.getInstance(insertionContext.project)
+                                                .autoPopupMemberLookup(insertionContext.editor, null)
+                                    }
                                 }
                         )
                     }
@@ -46,17 +63,23 @@ class WXSSPropertyValueCompletionContributor : CompletionContributor() {
                             context: ProcessingContext,
                             resultSet: CompletionResultSet
                     ) {
-                        CssElementDescriptorFactory.getDescriptor("text-align")?.let {
-                            it.allVariants.forEach { variant ->
-                                resultSet.addElement(
-                                        LookupElementBuilder.create(
-                                                variant
+                        val position = parameters.position
+                        val styleStatement = PsiTreeUtil.getParentOfType(position, WXSSStyleStatement::class.java)
+                        val propertyName = styleStatement?.firstChild?.text ?: return
+                        CssElementDescriptorFactory.getDescriptor(propertyName)?.let {
+                            it.allVariants.filter { variant -> variant !== null && (variant !is String || variant.isNotEmpty()) }
+                                    .forEach { variant ->
+                                        resultSet.addElement(
+                                                LookupElementBuilder.create(
+                                                        variant
+                                                )
                                         )
-                                )
-                            }
+                                    }
                         }
                     }
                 }
         )
+
+
     }
 }
