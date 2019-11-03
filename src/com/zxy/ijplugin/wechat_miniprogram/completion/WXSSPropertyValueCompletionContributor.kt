3c923969e885a468767bda75b08aaf384fe2520b
zxy <2544682494@qq.com>
2019-11-03 12:21:49 +0800

    对代码完成这块的摸索有进展

--- src/com/zxy/ijplugin/wechat_miniprogram/completion/WXSSPropertyValueCompletionContributor.kt
+++ src/com/zxy/ijplugin/wechat_miniprogram/completion/WXSSPropertyValueCompletionContributor.kt
@@ -1,8 +1,10 @@
 package com.zxy.ijplugin.wechat_miniprogram.completion
 
+import com.intellij.codeInsight.AutoPopupController
 import com.intellij.codeInsight.completion.*
 import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.patterns.PlatformPatterns
+import com.intellij.psi.css.impl.util.table.CssElementDescriptorFactory
 import com.intellij.util.ProcessingContext
 import com.zxy.ijplugin.wechat_miniprogram.lang.wxss.WXSSLanguage
 import com.zxy.ijplugin.wechat_miniprogram.lang.wxss.psi.WXSSTypes
@@ -12,7 +14,7 @@ class WXSSPropertyValueCompletionContributor : CompletionContributor() {
     init {
         extend(
                 CompletionType.BASIC,
-                PlatformPatterns. .psiElement(WXSSTypes.VALUE)
+                PlatformPatterns.psiElement(WXSSTypes.ATTRIBUTE_NAME)
                         .withLanguage(WXSSLanguage.INSTANCE),
                 object : CompletionProvider<CompletionParameters>() {
                     public override fun addCompletions(
@@ -20,7 +22,39 @@ class WXSSPropertyValueCompletionContributor : CompletionContributor() {
                             context: ProcessingContext,
                             resultSet: CompletionResultSet
                     ) {
-                        resultSet.addElement(LookupElementBuilder.create("Hello"))
+                        resultSet.addElement(
+                                LookupElementBuilder.create("Hello").withInsertHandler { insertionContext, lookupElement ->
+                                    val editor = insertionContext.editor
+                                    val offset = editor.caretModel.offset
+                                    insertionContext.document.insertString(offset, ": ;")
+                                    editor.caretModel.moveToOffset(offset + 2)
+                                    AutoPopupController.getInstance(insertionContext.project)
+                                            .autoPopupMemberLookup(insertionContext.editor, null)
+                                }
+                        )
+                    }
+                }
+        )
+
+        extend(
+                CompletionType.BASIC,
+                PlatformPatterns.psiElement(WXSSTypes.ATTRIBUTE_VALUE_LITERAL)
+                        .withLanguage(WXSSLanguage.INSTANCE),
+                object : CompletionProvider<CompletionParameters>() {
+                    public override fun addCompletions(
+                            parameters: CompletionParameters,
+                            context: ProcessingContext,
+                            resultSet: CompletionResultSet
+                    ) {
+                        CssElementDescriptorFactory.getDescriptor("text-align")?.let {
+                            it.allVariants.forEach { variant ->
+                                resultSet.addElement(
+                                        LookupElementBuilder.create(
+                                                variant
+                                        )
+                                )
+                            }
+                        }
                     }
                 }
         )
