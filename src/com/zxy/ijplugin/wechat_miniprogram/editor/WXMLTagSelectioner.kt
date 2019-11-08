acf419f3a193531dff60b860a4e0f0b88353aabd
2544682494@qq.com <2544682494@qq.com>
2019-11-08 14:33:44 +0800

    添加了双击选择wxml元素的行为控制

---
+++ src/com/zxy/ijplugin/wechat_miniprogram/editor/WXMLTagSelectioner.kt
@@ -0,0 +1,61 @@
+package com.zxy.ijplugin.wechat_miniprogram.editor
+
+import com.intellij.codeInsight.editorActions.wordSelection.AbstractWordSelectioner
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.util.TextRange
+import com.intellij.psi.PsiElement
+import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.psi.util.elementType
+import com.zxy.ijplugin.wechat_miniprogram.lang.wxml.psi.WXMLClosedElement
+import com.zxy.ijplugin.wechat_miniprogram.lang.wxml.psi.WXMLEndTag
+import com.zxy.ijplugin.wechat_miniprogram.lang.wxml.psi.WXMLStartTag
+import com.zxy.ijplugin.wechat_miniprogram.lang.wxml.psi.WXMLTypes
+
+/**
+ * 处理双击选择WXML元素开始标签的开始位置
+ * @see WXMLTypes.START_TAG_START
+ * @see com.intellij.codeInsight.editorActions.XmlTagSelectioner
+ * @see com.intellij.codeInsight.editorActions.HtmlSelectioner
+ */
+class WXMLStartTagSelectioner : AbstractWordSelectioner() {
+    override fun canSelect(psiElement: PsiElement): Boolean {
+        return psiElement.elementType == WXMLTypes.START_TAG_START
+    }
+
+    override fun select(
+            psiElement: PsiElement, charSequence: CharSequence, p2: Int, p3: Editor
+    ): MutableList<TextRange> {
+        val parent = psiElement.parent
+        val result = ArrayList<TextRange>(1)
+        if (parent is WXMLStartTag) {
+            result.add(parent.textRange)
+        } else if (parent is WXMLClosedElement) {
+            result.add(parent.textRange)
+        }
+        return result
+    }
+
+    override fun getMinimalTextRangeLength(element: PsiElement, text: CharSequence, cursorOffset: Int): Int {
+        return 2
+    }
+}
+
+/**
+ * 处理双击选择WXML元素结束标签的开始位置
+ * @see WXMLTypes.END_TAG_START
+ */
+class WXMLEndTagSelectioner : AbstractWordSelectioner() {
+    override fun canSelect(psiElement: PsiElement): Boolean {
+        return psiElement.elementType == WXMLTypes.END_TAG_START
+    }
+
+    override fun select(
+            psiElement: PsiElement, editorText: CharSequence, cursorOffset: Int, editor: Editor
+    ): MutableList<TextRange> {
+        return mutableListOf(PsiTreeUtil.getParentOfType(psiElement, WXMLEndTag::class.java)!!.textRange)
+    }
+
+    override fun getMinimalTextRangeLength(element: PsiElement, text: CharSequence, cursorOffset: Int): Int {
+        return 3
+    }
+}
