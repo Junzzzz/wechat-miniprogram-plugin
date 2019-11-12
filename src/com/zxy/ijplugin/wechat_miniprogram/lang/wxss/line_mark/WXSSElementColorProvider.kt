41c0bde025c1a66b8d3a20474877caccd5937e66
2544682494@qq.com <2544682494@qq.com>
2019-11-12 11:12:54 +0800

    wxss color 提供程序现在对颜色名称有效

--- src/com/zxy/ijplugin/wechat_miniprogram/lang/wxss/line_mark/WXSSElementColorProvider.kt
+++ src/com/zxy/ijplugin/wechat_miniprogram/lang/wxss/line_mark/WXSSElementColorProvider.kt
@@ -3,6 +3,7 @@ package com.zxy.ijplugin.wechat_miniprogram.lang.wxss.line_mark
 import com.intellij.openapi.editor.ElementColorProvider
 import com.intellij.psi.PsiElement
 import com.intellij.ui.ColorHexUtil
+import com.intellij.xml.util.ColorMap
 import com.zxy.ijplugin.wechat_miniprogram.lang.wxss.psi.WXSSTypes
 import com.zxy.ijplugin.wechat_miniprogram.lang.wxss.utils.WXSSElementFactory
 import java.awt.Color
@@ -18,6 +19,8 @@ class WXSSElementColorProvider : ElementColorProvider {
     override fun getColorFrom(psiElement: PsiElement): Color? {
         if (psiElement.node.elementType == WXSSTypes.HASH) {
             return ColorHexUtil.fromHex(psiElement.text)
+        } else if (psiElement.node.elementType == WXSSTypes.ATTRIBUTE_VALUE_LITERAL) {
+            return ColorMap.getColor(psiElement.text)
         }
         return null
     }
