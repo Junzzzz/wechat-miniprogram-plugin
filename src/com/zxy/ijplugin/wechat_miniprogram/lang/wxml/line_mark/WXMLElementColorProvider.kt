f24cf162cdd533a92c8765633fd38e152ee82575
2544682494@qq.com <2544682494@qq.com>
2019-11-12 11:34:14 +0800

    优化了WXML元素的颜色解析

--- src/com/zxy/ijplugin/wechat_miniprogram/lang/wxml/line_mark/WXMLElementColorProvider.kt
+++ src/com/zxy/ijplugin/wechat_miniprogram/lang/wxml/line_mark/WXMLElementColorProvider.kt
@@ -2,7 +2,9 @@ package com.zxy.ijplugin.wechat_miniprogram.lang.wxml.line_mark
 
 import com.intellij.openapi.editor.ElementColorProvider
 import com.intellij.psi.PsiElement
+import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.xml.util.ColorMap
+import com.zxy.ijplugin.wechat_miniprogram.lang.wxml.psi.WXMLAttribute
 import com.zxy.ijplugin.wechat_miniprogram.lang.wxml.psi.WXMLStringText
 import com.zxy.ijplugin.wechat_miniprogram.lang.wxml.utils.WXMLElementFactory
 import java.awt.Color
@@ -14,7 +16,11 @@ class WXMLElementColorProvider : ElementColorProvider {
     }
 
     override fun getColorFrom(psiElement: PsiElement): Color? {
-        if (psiElement is WXMLStringText) {
+        if (psiElement is WXMLStringText && PsiTreeUtil.getParentOfType(
+                        psiElement, WXMLAttribute::class.java
+                )?.name?.contains("color") == true) {
+            // 在属性值的字符串中
+            // 并且属性包含color
             return ColorMap.getColor(psiElement.text)
         }
         return null
