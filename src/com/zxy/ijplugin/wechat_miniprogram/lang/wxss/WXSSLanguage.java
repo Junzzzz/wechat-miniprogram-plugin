f31950e5870ddaa32303b96e77437057ffb16902
zxy <2544682494@qq.com>
2019-11-17 14:51:18 +0800

    新增 - WXSS自动完成数字单位

--- src/com/zxy/ijplugin/wechat_miniprogram/lang/wxss/WXSSLanguage.java
+++ src/com/zxy/ijplugin/wechat_miniprogram/lang/wxss/WXSSLanguage.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 public class WXSSLanguage extends Language {
 
-    public static List<String> UNITS = Arrays.asList("rpx","px","%","deg","s","rgb","rect","ex","rem","Hz","pt","rad","in","kHz","mm","ms","cm");
+    public static List<String> UNITS = Arrays.asList("rpx","px","%","deg","s","rgb","rect","ex","rem","Hz","pt","rad","in","kHz","mm","ms","cm","vw","vh","vmin","vmax","ch","q","qt");
 
     public static WXSSLanguage INSTANCE = new WXSSLanguage();
 
