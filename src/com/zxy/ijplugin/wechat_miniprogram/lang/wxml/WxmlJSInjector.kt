522c3262393bb9bff193a3163634d11bf9131dba
2544682494@qq.com <2544682494@qq.com>
2019-11-21 15:55:33 +0800

    添加了对wxs标签的属性的处理

--- src/com/zxy/ijplugin/wechat_miniprogram/lang/wxml/WxmlJSInjector.kt
+++ src/com/zxy/ijplugin/wechat_miniprogram/lang/wxml/WxmlJSInjector.kt
@@ -23,33 +23,56 @@ class WxmlJSInjector : MultiHostInjector {
     }
 
     override fun getLanguagesToInject(multiHostRegistrar: MultiHostRegistrar, psiElement: PsiElement) {
-        if (psiElement is WXMLText) {
-            val wxmlOpenedElement = PsiTreeUtil.getParentOfType(psiElement, WXMLOpenedElement::class.java)
-            if (wxmlOpenedElement != null) {
-                if ((wxmlOpenedElement.parent as WXMLElement).tagName == "wxs") {
-                    // 对wxs标签注入js语言
-                    multiHostRegistrar.startInjecting(WxmlJsLanguage.INSTANCE)
-                            .addPlace(null, null, psiElement, TextRange(0, psiElement.textLength))
-                            .doneInjecting()
-                } else {
-                    // 对元素内容中的双括号注入js语言
-                    searchDoubleBraceAndInject(psiElement, multiHostRegistrar)
-                }
+        when (psiElement) {
+            is WXMLText -> {
+                injectInText(psiElement, multiHostRegistrar)
             }
-        } else if (psiElement is WXMLStringText) {
-            if (PsiTreeUtil.getParentOfType(psiElement, WXMLAttribute::class.java)?.let {
-                        it.isEventHandler()
-                    } == true && !DOUBLE_BRACE_REGEX.matches(psiElement.text)) {
-                // 此属性是事件
-                // 并且属性值中没有双括号
+            is WXMLStringText -> {
+                injectInStringText(psiElement, multiHostRegistrar)
+            }
+        }
+    }
+
+    private fun injectInStringText(
+            psiElement: WXMLStringText,
+            multiHostRegistrar: MultiHostRegistrar
+    ) {
+        val element = PsiTreeUtil.getParentOfType(psiElement, WXMLElement::class.java)
+        if (element == null || element.tagName == "wxs") {
+            // wxs 标签的属性不支持 {{}} 语法
+            return
+        }
+        if (PsiTreeUtil.getParentOfType(psiElement, WXMLAttribute::class.java)?.let {
+                    it.isEventHandler()
+                } == true && !DOUBLE_BRACE_REGEX.matches(psiElement.text)) {
+            // 此属性是事件
+            // 并且属性值中没有双括号
+            multiHostRegistrar.startInjecting(WxmlJsLanguage.INSTANCE)
+                    .addPlace(null, null, psiElement, TextRange(0, psiElement.textLength))
+                    .doneInjecting()
+        } else {
+            // 对字符串中的双括号注入js语言
+            searchDoubleBraceAndInject(psiElement, multiHostRegistrar)
+        }
+    }
+
+    private fun injectInText(
+            psiElement: WXMLText,
+            multiHostRegistrar: MultiHostRegistrar
+    ) {
+        val wxmlOpenedElement = PsiTreeUtil.getParentOfType(psiElement, WXMLOpenedElement::class.java)
+        if (wxmlOpenedElement != null) {
+            if ((wxmlOpenedElement.parent as WXMLElement).tagName == "wxs") {
+                // 对wxs标签注入js语言
                 multiHostRegistrar.startInjecting(WxmlJsLanguage.INSTANCE)
                         .addPlace(null, null, psiElement, TextRange(0, psiElement.textLength))
                         .doneInjecting()
-            } else {
-                // 对字符串中的双括号注入js语言
-                searchDoubleBraceAndInject(psiElement, multiHostRegistrar)
+                return
             }
         }
+
+        // 对文本中的双括号注入js语言
+        searchDoubleBraceAndInject(psiElement, multiHostRegistrar)
     }
 }
 
