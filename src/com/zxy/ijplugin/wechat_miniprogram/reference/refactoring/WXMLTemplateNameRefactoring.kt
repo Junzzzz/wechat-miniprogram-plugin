75880ac31b187d8b12ef75be9df8bc334437f3d7
zxy <2544682494@qq.com>
2019-11-22 16:37:02 +0800

    对template 的name属性 提供就地重构

--- src/com/zxy/ijplugin/wechat_miniprogram/reference/refactoring/WXMLTemplateNameRefactoring.kt
+++ src/com/zxy/ijplugin/wechat_miniprogram/reference/refactoring/WXMLTemplateNameRefactoring.kt
@@ -2,12 +2,17 @@ package com.zxy.ijplugin.wechat_miniprogram.reference.refactoring
 
 import com.intellij.lang.refactoring.RefactoringSupportProvider
 import com.intellij.psi.PsiElement
-import com.zxy.ijplugin.wechat_miniprogram.lang.wxss.psi.WXSSStringText
+import com.intellij.psi.util.PsiTreeUtil
+import com.zxy.ijplugin.wechat_miniprogram.lang.wxml.psi.WXMLAttribute
+import com.zxy.ijplugin.wechat_miniprogram.lang.wxml.psi.WXMLElement
+import com.zxy.ijplugin.wechat_miniprogram.lang.wxml.psi.WXMLStringText
 
 class WXMLTemplateNameRefactoring : RefactoringSupportProvider() {
 
     override fun isMemberInplaceRenameAvailable(element: PsiElement, context: PsiElement?): Boolean {
-        return element is WXSSStringText
+        return element is WXMLStringText && PsiTreeUtil.getParentOfType(element, WXMLAttribute::class.java)?.let {
+            it.name == "name" && PsiTreeUtil.getParentOfType(it, WXMLElement::class.java)?.tagName == "template"
+        } == true
     }
 
 }
