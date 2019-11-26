b594e5841b1b0894f267ad6ee9b27686c5371b8d
2544682494@qq.com <2544682494@qq.com>
2019-11-26 17:47:47 +0800

    完成页面配置文件的schema提供

--- src/com/zxy/ijplugin/wechat_miniprogram/completion/WechatMiniProgramJsonSchemaProviderFactory.kt
+++ src/com/zxy/ijplugin/wechat_miniprogram/completion/WechatMiniProgramJsonSchemaProviderFactory.kt
@@ -1,25 +1,35 @@
 package com.zxy.ijplugin.wechat_miniprogram.completion
 
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ProjectFileIndex
 import com.intellij.openapi.vfs.VirtualFile
 import com.jetbrains.jsonSchema.extension.JsonSchemaFileProvider
 import com.jetbrains.jsonSchema.extension.JsonSchemaProviderFactory
 import com.jetbrains.jsonSchema.extension.SchemaType
+import com.zxy.ijplugin.wechat_miniprogram.context.isWechatMiniProgramContext
 
-class WechatMiniProgramJsonSchemaProviderFactory:JsonSchemaProviderFactory {
+class WechatMiniProgramJsonSchemaProviderFactory : JsonSchemaProviderFactory {
     override fun getProviders(project: Project): MutableList<JsonSchemaFileProvider> {
-        return mutableListOf(WechatMiniProgramProjectConfigJsonSchemaFileProvider())
+        return mutableListOf(AppJsonSchemaFileProvider(project), PageJsonSchemaFileProvider(project))
     }
 }
 
-class WechatMiniProgramProjectConfigJsonSchemaFileProvider : JsonSchemaFileProvider {
+fun isAppJsonFile(project: Project, virtualFile: VirtualFile): Boolean {
+    val contentRoot = ProjectFileIndex.SERVICE.getInstance(
+            project
+    ).getContentRootForFile(virtualFile)
+    // 在根目录下且名称为 app.json
+    return virtualFile.parent == contentRoot && virtualFile.name == "app.json"
+}
+
+private class AppJsonSchemaFileProvider(private val project: Project) : JsonSchemaFileProvider {
 
     override fun getName(): String {
         return "app.json"
     }
 
     override fun isAvailable(virtualFile: VirtualFile): Boolean {
-        return virtualFile.name == "app.json"
+        return isAppJsonFile(project, virtualFile)
     }
 
     override fun getSchemaFile(): VirtualFile? {
@@ -33,4 +43,25 @@ class WechatMiniProgramProjectConfigJsonSchemaFileProvider : JsonSchemaFileProvi
         return SchemaType.schema
     }
 
+}
+
+private class PageJsonSchemaFileProvider(private val project: Project) : JsonSchemaFileProvider {
+    override fun getName(): String {
+        return "page.js"
+    }
+
+    override fun isAvailable(virtualFile: VirtualFile): Boolean {
+        return isWechatMiniProgramContext(this.project) && !isAppJsonFile(project, virtualFile)
+    }
+
+    override fun getSchemaFile(): VirtualFile? {
+        return JsonSchemaProviderFactory.getResourceFile(
+                this.javaClass,
+                "/jsonSchemas/page.json"
+        )
+    }
+
+    override fun getSchemaType(): SchemaType {
+        return SchemaType.schema
+    }
 }
