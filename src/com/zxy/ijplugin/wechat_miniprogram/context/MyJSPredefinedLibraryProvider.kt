baeb88c15fc5bfc9f6ff10ab33ee695960d3292d
2544682494@qq.com <2544682494@qq.com>
2020-01-06 17:27:49 +0800

    修复首次安装可能会找不到js库文件的bug

--- src/com/zxy/ijplugin/wechat_miniprogram/context/MyJSPredefinedLibraryProvider.kt
+++ src/com/zxy/ijplugin/wechat_miniprogram/context/MyJSPredefinedLibraryProvider.kt
@@ -76,6 +76,7 @@ package com.zxy.ijplugin.wechat_miniprogram.context
 import com.intellij.lang.javascript.library.JSPredefinedLibraryProvider
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VfsUtil
+import com.intellij.util.ResourceUtil
 import com.intellij.webcore.libraries.ScriptingLibraryModel
 
 class MyJSPredefinedLibraryProvider : JSPredefinedLibraryProvider() {
@@ -86,7 +87,7 @@ class MyJSPredefinedLibraryProvider : JSPredefinedLibraryProvider() {
 
     override fun getPredefinedLibraries(project: Project): Array<out ScriptingLibraryModel> {
         if (isWechatMiniProgramContext(project)){
-            val fileUrl = this.javaClass.getResource("/wx.ts")
+            val fileUrl = ResourceUtil.getResource(javaClass,"/","wx.ts")
             return arrayOf(
                     ScriptingLibraryModel.createPredefinedLibrary(
                             "wechat-mini-program-api", arrayOf(VfsUtil.findFileByURL(fileUrl)), true
