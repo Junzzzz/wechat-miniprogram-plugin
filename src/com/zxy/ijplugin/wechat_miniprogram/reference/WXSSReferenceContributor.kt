9022f7ee1408d7adec5740091db92171936f6ce6
2544682494@qq.com <2544682494@qq.com>
2020-03-31 16:54:27 +0800

    改造wxss @import的文件引用

--- src/com/zxy/ijplugin/wechat_miniprogram/reference/WXSSReferenceContributor.kt
+++ src/com/zxy/ijplugin/wechat_miniprogram/reference/WXSSReferenceContributor.kt
@@ -75,29 +75,29 @@ package com.zxy.ijplugin.wechat_miniprogram.reference
 
 import com.intellij.patterns.PlatformPatterns
 import com.intellij.psi.*
+import com.intellij.psi.css.CssImport
+import com.intellij.psi.css.CssString
 import com.intellij.psi.css.CssTerm
 import com.intellij.psi.css.impl.CssElementTypes
 import com.intellij.psi.css.impl.util.CssUtil
 import com.intellij.psi.impl.source.resolve.reference.impl.providers.FileReferenceSet
 import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.util.ProcessingContext
-import com.zxy.ijplugin.wechat_miniprogram.lang.wxss.psi.WXSSImport
-import com.zxy.ijplugin.wechat_miniprogram.lang.wxss.psi.WXSSStringText
 
 class WXSSReferenceContributor : PsiReferenceContributor() {
     override fun registerReferenceProviders(psiReferenceRegistrar: PsiReferenceRegistrar) {
         // 解析@import字符串中的路径
-        psiReferenceRegistrar.registerReferenceProvider(PlatformPatterns.psiElement(WXSSStringText::class.java),
+        psiReferenceRegistrar.registerReferenceProvider(
+                PlatformPatterns.psiElement(CssString::class.java),
                 object : PsiReferenceProvider() {
                     override fun getReferencesByElement(
                             psiElement: PsiElement, p1: ProcessingContext
                     ): Array<out PsiReference> {
-                        if (PsiTreeUtil.getParentOfType(psiElement, WXSSImport::class.java) != null) {
+                        if (PsiTreeUtil.getParentOfType(psiElement, CssImport::class.java) != null) {
                             return FileReferenceSet(psiElement).allReferences
                         }
                         return PsiReference.EMPTY_ARRAY
                     }
-
                 }
         )
 
