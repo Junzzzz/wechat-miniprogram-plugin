2a1eb22fb433db35e6332114867c4a4732e5ce5d
2544682494@qq.com <2544682494@qq.com>
2020-03-31 17:46:39 +0800

    wxss重命名还有问题

--- src/com/zxy/ijplugin/wechat_miniprogram/reference/manipulator/WXMLClassSelectorManipulator.kt
+++ src/com/zxy/ijplugin/wechat_miniprogram/reference/manipulator/WXMLClassSelectorManipulator.kt
@@ -73,12 +73,14 @@
 
 package com.zxy.ijplugin.wechat_miniprogram.reference.manipulator
 
-import com.zxy.ijplugin.wechat_miniprogram.lang.wxss.psi.WXSSClassSelector
-import com.zxy.ijplugin.wechat_miniprogram.lang.wxss.utils.WXSSElementFactory
+import com.intellij.psi.css.CssClass
+import com.intellij.psi.css.CssElementFactory
+import com.zxy.ijplugin.wechat_miniprogram.lang.wxss.WXSSLanguage
+import com.zxy.ijplugin.wechat_miniprogram.utils.findChildOfType
 
 class WXMLClassSelectorManipulator :
-        MyAbstractElementManipulator<WXSSClassSelector>({ project, elementText ->
-            WXSSElementFactory.createClassSelector(
-                    project, elementText
-            )
+        MyAbstractElementManipulator<CssClass>({ project, elementText ->
+            CssElementFactory.getInstance(
+                    project
+            ).createSimpleSelector(elementText, WXSSLanguage.INSTANCE).findChildOfType()!!
         })
--- src/com/zxy/ijplugin/wechat_miniprogram/reference/refactoring/WXSSSelectorRefactoring.kt
+++ src/com/zxy/ijplugin/wechat_miniprogram/reference/refactoring/WXSSSelectorRefactoring.kt
@@ -75,13 +75,13 @@ package com.zxy.ijplugin.wechat_miniprogram.reference.refactoring
 
 import com.intellij.lang.refactoring.RefactoringSupportProvider
 import com.intellij.psi.PsiElement
-import com.zxy.ijplugin.wechat_miniprogram.lang.wxss.psi.WXSSClassSelector
-import com.zxy.ijplugin.wechat_miniprogram.lang.wxss.psi.WXSSIdSelector
+import com.intellij.psi.css.CssClass
+import com.intellij.psi.css.CssIdSelector
 
 class WXSSSelectorRefactoring : RefactoringSupportProvider() {
 
 
     override fun isMemberInplaceRenameAvailable(element: PsiElement, context: PsiElement?): Boolean {
-        return element is WXSSIdSelector || element is WXSSClassSelector
+        return element is CssIdSelector || element is CssClass
     }
 }
