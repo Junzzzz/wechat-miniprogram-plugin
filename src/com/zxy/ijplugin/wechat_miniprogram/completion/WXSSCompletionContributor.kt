0c2b7fc4973af30a70397906f5a33c671b25b923
2544682494@qq.com <2544682494@qq.com>
2020-04-01 16:33:22 +0800

    自动完成wxss class时不会带.

--- src/com/zxy/ijplugin/wechat_miniprogram/completion/WXSSCompletionContributor.kt
+++ src/com/zxy/ijplugin/wechat_miniprogram/completion/WXSSCompletionContributor.kt
@@ -137,7 +137,7 @@ class WXSSCompletionContributor : CompletionContributor() {
                                         .map {
                                             it.rangeInElement.substring(it.element.text)
                                         }.distinct().map {
-                                            LookupElementBuilder.create(it).withPresentableText(".$it")
+                                            LookupElementBuilder.create(it).withPresentableText("$it")
                                                     .withIcon(AllIcons.Xml.Css_class)
                                         }.toMutableList()
                         )
