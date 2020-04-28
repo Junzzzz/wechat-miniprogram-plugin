5affcf2c40059dc0fe2f07c9342c140e5550fda1
2544682494@qq.com <2544682494@qq.com>
2020-04-28 14:46:39 +0800

    移动文件位置

--- src/com/zxy/ijplugin/wechat_miniprogram/lang/sass/MiniProgramSassFormattingModelBuilder.kt
+++ src/com/zxy/ijplugin/wechat_miniprogram/lang/sass/MiniProgramSassFormattingModelBuilder.kt
@@ -71,19 +71,72 @@
  *    See the Mulan PSL v1 for more details.
  */
 
-package com.zxy.ijplugin.wechat_miniprogram.lang.scss
+package com.zxy.ijplugin.wechat_miniprogram.lang.sass
 
-import com.intellij.lexer.Lexer
-import com.intellij.psi.css.impl.util.scheme.CssElementDescriptorFactory2
-import org.jetbrains.plugins.sass.highlighting.SASSSyntaxHighlighter
-import org.jetbrains.plugins.sass.lexer.SASSHighlighterLexer
+import com.intellij.formatting.Alignment
+import com.intellij.formatting.Indent
+import com.intellij.lang.ASTNode
+import com.intellij.psi.codeStyle.CodeStyleSettings
+import com.intellij.psi.css.CssBlock
+import com.intellij.psi.util.PsiTreeUtil
+import com.zxy.ijplugin.wechat_miniprogram.lang.utils.StyleLanguageUtils
+import com.zxy.ijplugin.wechat_miniprogram.lang.wxss.formatter.WXSSPropertyBlock
+import com.zxy.ijplugin.wechat_miniprogram.lang.wxss.formatter.WXSSTermListBlock
+import org.jetbrains.plugins.sass.formatter.SassFormattingModelBuilder
+import org.jetbrains.plugins.sass.psi.SASSElementTypes
+import org.jetbrains.plugins.sass.settings.SassCodeStyleSettings
+import org.jetbrains.plugins.scss.SCSSLanguage
+import org.jetbrains.plugins.scss.psi.SassScssFunctionBodyImpl
 
-class MiniProgramSassSyntaxHighlighter : SASSSyntaxHighlighter() {
+class MiniProgramSassFormattingModelBuilder : SassFormattingModelBuilder() {
 
-    override fun getHighlightingLexer(): Lexer {
-        return SASSHighlighterLexer(CssElementDescriptorFactory2.getInstance().valueIdentifiers.apply {
-            this.add("rpx")
-        })
+    override fun createExtension(settings: CodeStyleSettings): SassFormattingExtension {
+        return object : SassFormattingModelBuilder.SassFormattingExtension(
+                settings.getCommonSettings(SCSSLanguage.INSTANCE),
+                settings.getCustomSettings(SassCodeStyleSettings::class.java)
+        ) {
+            override fun createPropertyBlock(
+                    _node: ASTNode, extension: CssFormattingExtension?, alignment: Alignment?,
+                    childAlignment: Alignment?
+            ): CssPropertyBlock {
+                return if (StyleLanguageUtils.isMiniProgramContext(_node)) {
+                    val indent = if (_node.treeParent
+                                    .elementType !== SASSElementTypes.PROPERTY_RULESET && (PsiTreeUtil.getParentOfType(
+                                    _node.psi,
+                                    CssBlock::class.java
+                            ) != null || PsiTreeUtil.getParentOfType(
+                                    _node.psi,
+                                    SassScssFunctionBodyImpl::class.java
+                            ) != null)) Indent.getNormalIndent(
+                            true
+                    ) else Indent.getNoneIndent()
+                    WXSSPropertyBlock(_node, indent, extension, alignment, childAlignment)
+                } else {
+                    super.createPropertyBlock(_node, extension, alignment, childAlignment)
+                }
+            }
+
+            override fun createPropertyBlock(
+                    _node: ASTNode?, indent: Indent?, extension: CssFormattingExtension?, alignment: Alignment?,
+                    childAlignment: Alignment?
+            ): CssPropertyBlock {
+                return if (StyleLanguageUtils.isMiniProgramContext(_node)) {
+                    WXSSPropertyBlock(_node, indent, extension, alignment, childAlignment)
+                } else {
+                    super.createPropertyBlock(_node, indent, extension, alignment, childAlignment)
+                }
+            }
+
+            override fun createTermListBlock(
+                    _node: ASTNode?, indent: Indent?, alignment: Alignment?, shouldIndentContent: Boolean
+            ): CssTermListBlock {
+                return if (StyleLanguageUtils.isMiniProgramContext(_node)) {
+                    WXSSTermListBlock(_node, indent, this, alignment, shouldIndentContent)
+                } else {
+                    super.createTermListBlock(_node, indent, alignment, shouldIndentContent)
+                }
+            }
+        }
     }
 
 }
